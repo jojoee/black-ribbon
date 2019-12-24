f02f23a56a5688d0f53bcefd49f3e2ecad0a8bfc
Nathachai Thongniran <inid3a@gmail.com>
2019-12-24 16:37:11 +0700

    Add pack.zip gulp task to create submission package for WordPress SVN

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect-php'),
+  zip = require('gulp-zip'),
+  clean = require('gulp-clean'),
+  fs = require('fs'),
   browserSync = require('browser-sync');
+
+const mainFile = 'black-ribbon.php';
+const mainFileContent = fs.readFileSync(mainFile, 'utf8');
+const pluginVersion = /^Version.*$/igm.exec(mainFileContent)[0].substring(9).trim();
+const pluginName = /^Plugin Name.*$/igm.exec(mainFileContent)[0].substring(13).trim().toLowerCase();
+const packageFolderName = pluginName + '-' + pluginVersion;
 
 gulp.task('connect-sync', function() {
   connect.server({}, function() {
@@ -15,6 +24,29 @@ gulp.task('connect-sync', function() {
   ]).on('change', function() {
     browserSync.reload();
   });
+});
+
+gulp.task('clean', function () {
+  return gulp.src(pluginName + '-*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('pack', ['clean'], function () {
+  return gulp.src([
+    'css/**',
+    'image/**',
+    'black-ribbon.php',
+    'readme.txt',
+    'screenshot-1.jpg',
+    'screenshot-2.jpg'
+  ], {base: '.'})
+    .pipe(gulp.dest(packageFolderName));
+});
+
+gulp.task('pack.zip', ['pack'], function () {
+  return gulp.src(packageFolderName + '/**')
+    .pipe(zip(packageFolderName + '.zip'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['connect-sync']);
